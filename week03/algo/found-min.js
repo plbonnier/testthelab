da2c0a77f2fd033b57b0ccc1182539dc1316660f
YoussefSlim <slimyoussef9511@gmail.com>
2024-05-28 09:43:57 +0200

    JS | add others algo

---
+++ week03/algo/found-min.js
@@ -0,0 +1,13 @@
+/*
+Create a function `min` which founds the minimum value of a number array.
+
+If the array is empty or null, return null.
+
+Example:
+* [1, 2, 3] -> 1
+* [3, -2, 1] -> -2
+* [] -> null
+
+You can't use the function Math.min()
+
+*/
