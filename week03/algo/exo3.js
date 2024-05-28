f07b62b85175c65ed16b5f9875975a0d5b2c1865
YoussefSlim <slimyoussef9511@gmail.com>
2024-05-28 14:22:56 +0200

    JS | correction algo | exo3

--- week03/algo/exo3.js
+++ week03/algo/exo3.js
@@ -1,8 +1,13 @@
 // Implémentez l'algorithme suivant :
 // Demander Quel est votre livre préféré ?
-
+const book = prompt("Quel est votre livre préféré ?").toLowerCase();
 // SI le livre n'est pas Dune
+if (book !== "Dune".toLowerCase()) {
+  // ALORS afficher oui
 
-// ALORS afficher oui bon ... personne n'est parfait
+  alert("bon ... personne n'est parfait");
+} else {
+  // SINON afficher aaahhh voilà quelqu'un de bien !
 
-// SINON afficher aaahhh voilà quelqu'un de bien !
+  alert("aaahhh voilà quelqu'un de bien !");
+}
