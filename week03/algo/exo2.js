83e75c5484e0134e58e956a809db68ed7a2a5736
YoussefSlim <slimyoussef9511@gmail.com>
2024-05-28 14:10:42 +0200

    JS | correction algo | exo2

--- week03/algo/exo2.js
+++ week03/algo/exo2.js
@@ -1,2 +1,6 @@
 // écrivez une fonction "printMultiply" qui attend 2 paramètres
 // "nb1" et "nb2", et qui affiche en console le résultat de leur multiplication.
+
+const printMultiply = (a, b) => a * b;
+
+console.log(printMultiply(2, 4));
