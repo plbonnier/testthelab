02f286fbd9dd71c2c95233a0f3f6ded4e41603c2
YoussefSlim <slimyoussef9511@gmail.com>
2024-05-28 14:51:21 +0200

    JS | correction algo | exo5

--- week03/algo/exo5.js
+++ week03/algo/exo5.js
@@ -6,3 +6,12 @@
 // - si le prénom contient moins de 8 lettres, afficher "vous avez un prénom court !"
 
 // - sinon , afficher "votre prénom est long !"
+
+const demande = prompt("Votre nom svp");
+if (demande === "" || demande === "juste") {
+  alert("vous n'avez pas de prenom?");
+} else if (demande.length < 8) {
+  alert("vous avez un prénom court !");
+} else {
+  alert("votre prénom est long !");
+}
