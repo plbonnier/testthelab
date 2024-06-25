7f21042fb43c85403aa8d63e17c9a35b36d94baa
YoussefSlim <slimyoussef9511@gmail.com>
2024-06-25 12:19:05 +0200

    correction todolist | test E2E| refacto | cypress env | hooks

--- week07/ToDoList/cypress.config.js
+++ week07/ToDoList/cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    basUrl: "http://127.0.0.1:5500/week07/ToDoList/index.html",
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
