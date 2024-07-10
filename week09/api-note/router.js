bd99615680b2a3b57bc2ca178e4e511f74a5e7e9
plbonnier <pl_bonnier@yahoo.fr>
2024-07-10 09:09:41 +0200

    router.js

--- week09/api-note/router.js
+++ week09/api-note/router.js
@@ -59,6 +59,43 @@ router.get("/users/:id", async (req, res) => {
   }
 });
 
+router.put("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email, password } = req.body;
+
+    const user = await db.query(
+      "update user set name = ?, email = ?, password = ? where id = ?",
+    [name, email, password, id]
+    );
+    res.status(200).json({
+      status: 200,
+      message: "user updated",
+      user : req.body.name,
+    });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await db.query(
+      "delete from user where id = ?",
+      [id]
+    );
+
+    res.status(200).json({
+      status: 200,
+      message: "user deleted",
+    });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
 router.post("/notes", async (req, res) => {
   try {
     const { title, description, category, id_user } = req.body;
@@ -79,4 +116,45 @@ router.get("/notes", async (req, res) => {
     res.status(200).json(notes[0]);
   } catch (error) {}
 });
+
+router.put("/notes/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, category, id_user } = req.body;
+
+    const note = await db.query(
+      "update note set title = ?, description = ?, category = ?, id_user = ? where id = ?",
+      [title, description, category, id_user, id]
+    );
+
+    res.status(200).json({
+      status: 200,
+      message: "note updated",
+      note : [ {title : req.body.title,
+      decription : req.body.description,
+      category: req.body.category}],
+    });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+router.delete("/notes/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const note = await db.query(
+      "delete from note where id = ?",
+      [id]
+    );
+
+    res.status(200).json({
+      status: 200,
+      message: "note deleted",
+    });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
 module.exports = router;
